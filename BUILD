1bfa0e273fb9fc3e4ba27464bc317b3d54d8aa77
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-01-02 00:59:39 +0000

    initial commit

---
+++ BUILD
@@ -0,0 +1,30 @@
+REQUIREMENTS:
+
+You will need either GNU, Allegro, CMU, or Lucid Common Lisp to run
+NESL.  CMU and GNU Common Lisp are available for free.  See the FTP site
+at ftp://ftp.ma.utexas.edu/pub/gcl/ for information on GCL, or email
+slisp@cs.cmu.edu for information on CMU CL.  You will also need a
+Unix-like operating system (SunOS, AIX, HP-UX, Ultrix, Mach and Unicos
+work fine).
+
+For the full NESL release you will also need an C compiler, lex, yacc,
+and if you want the graphics routines, an X11 library.
+
+HOW TO BUILD NESL:
+
+The following steps should be sufficient to build a version of NESL
+to run on your local workstation.
+
+  o Run "make" from the top-level NESL directory.  This builds CVL,
+    VCODE, and xneslplot, leaving "vinterp.serial" and "xneslplot" in
+    the "bin" directory.
+
+  o Start a Common Lisp in the top-level Nesl directory, and enter: 
+       (load "neslsrc/build.lisp")
+
+  o Follow the instructions for dumping an executable version
+    of Nesl.  This will create a file "bin/runnesl", which can
+    be executed directly to start up NESL.
+
+If you have problems, or for further details, see the user's manual in
+doc/user.ps.
---
+++ utils/xneslplot.h
@@ -0,0 +1,42 @@
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+
+#define DEFAULT_TITLE "NESL Graphics Display"
+#define ICONNAME "NESLG"
+#define name_width 8
+#define name_height 8
+#define name_x_hot 8
+#define name_y_hot 8
+
+Display *display;
+Window win;
+GC graphcontext;
+Pixmap pxmap;
+int window_height;
+char shade_bits[8] ={
+0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+char *default_font_name = "6x13";
+
+char *ps_header1 =
+"%!PS-Adobe-1.0\n%%Title:\n%%Creator: Nesl\n%%CreationDate:\n%%Pages: 1\n%%DocumentFonts:\n";
+
+char *ps_header2 =
+"%%EndComments\n/spoofdict 25 dict def\nspoofdict begin\n/S {moveto lineto stroke} bind def\n/LS {moveto} bind def\n/L {2 copy lineto stroke moveto} bind def\n/P {moveto 0 0 rlineto stroke} bind def\n/F {findfont exch scalefont setfont} bind def\n/T {gsave moveto rotate show grestore} bind def\n%%EndProlog\n2 setlinecap\n0 setgray\n8 /Times-Roman F\n";
+
+char *ps_tailer =
+"showpage\nend\n";
+
+extern void begin_paren_vector();
+extern void end_paren_vector();
+extern void end_paren_vector_with_space();
+extern int get_number();
+extern char *get_string();
+extern int sum();
+extern int read_int();
+extern int *read_int_array();
+extern char *read_char_array();
+extern XPoint *read_points();
+extern XSegment *read_segments();
+extern XArc *read_arcs();
+extern void put_number();
